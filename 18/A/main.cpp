b6b3eab0afb0977eac214eaa9f83dc59008ad020
dburl <burlyaev.dmitry@gmail.com>
2017-01-18 00:40:59 +0100

    18A fixed -double/float

--- 18/A/main.cpp
+++ 18/A/main.cpp
@@ -5,25 +5,28 @@
 
 using namespace std;
 
-
-bool prob(float a, float b, float c){
-	return a*a+b*b==c*c || c*c+b*b==a*a || a*a+c*c==b*b;
+bool degenerate(const vector<long>& x, const vector<long>& y){
+	return (y[1]-y[2])*(x[0]-x[1])== (y[0]-y[1])*(x[1]-x[2]);
 }
 
-bool right(vector<int>& x, vector<int>& y){
-	float d1= sqrt( (x[0]-x[1])*(x[0]-x[1])+(y[0]-y[1])*(y[0]-y[1]) );
-	float d2= sqrt((x[1]-x[2])*(x[1]-x[2])+(y[1]-y[2])*(y[1]-y[2]));
-	float d3= sqrt((x[2]-x[0])*(x[2]-x[0])+(y[2]-y[0])*(y[2]-y[0]));
+bool prob(long a, long b, long c){
+	return (a+b)==c || (c+b)==a || (a+c)==b;
+}
+
+bool right(const vector<long>& x, const vector<long>& y){
+	long d1= ( (x[0]-x[1])*(x[0]-x[1])+(y[0]-y[1])*(y[0]-y[1]) );
+	long d2= ((x[1]-x[2])*(x[1]-x[2])+(y[1]-y[2])*(y[1]-y[2]));
+	long d3= ((x[2]-x[0])*(x[2]-x[0])+(y[2]-y[0])*(y[2]-y[0]));
 	return prob(d1,d2,d3);
 }
-bool almostRigth(vector<int>& x, vector<int>& y){
-	vector<vector<int> > xxx;
-	vector<vector<int> > yyy;
+bool almostRigth(const vector<long>& x, const vector<long>& y){
+	vector<vector<long> > xxx;
+	vector<vector<long> > yyy;
 	for (int i=0;i<3;++i){
-		vector<int> tmpXP=x;
-		vector<int> tmpXM=x;
-		vector<int> tmpYP=y;
-		vector<int> tmpYM=y;
+		vector<long> tmpXP=x;
+		vector<long> tmpXM=x;
+		vector<long> tmpYP=y;
+		vector<long> tmpYM=y;
 		//
 		tmpXP[i]+=1; xxx.push_back(tmpXP); yyy.push_back(y);
 		tmpXM[i]-=1; xxx.push_back(tmpXM); yyy.push_back(y);
@@ -31,8 +34,10 @@ bool almostRigth(vector<int>& x, vector<int>& y){
 		tmpYM[i]-=1; xxx.push_back(x); yyy.push_back(tmpYM);
 	}
 	for (int i=0;i<xxx.size();++i){
-		if (right(xxx[i],yyy[i])){
-			cout<<"ALMOST"<<endl;
+		if (right(xxx[i],yyy[i])&& !degenerate(xxx[i],yyy[i])){
+			//for (int j=0;j<3;++i){
+			//cout<< xxx[i][j]<<" "<<yyy[i][j]<<endl;
+			//}
 			return true;	
 		}	
 	}
@@ -43,16 +48,15 @@ bool almostRigth(vector<int>& x, vector<int>& y){
 
 
 int  main(){
-	vector<int> x(3),y(3);
+	vector<long> x(3),y(3);
 	cin>>x[0]>>y[0]>>x[1]>>y[1]>>x[2]>>y[2];
 	//
 	if (right(x,y)){
 		cout<<"RIGHT"<<endl;
 		return 0;	
-	}
-	if (!almostRigth(x,y)) {cout<<"NEITHER"<<endl;}
+	} else	if (almostRigth(x,y)) {
+		cout<<"ALMOST"<<endl; return 0;
+	} else {cout<<"NEITHER"<<endl;}
 	
-
-
 	return 0;
 }
--- 18/B/main.cpp
+++ 18/B/main.cpp
@@ -3,30 +3,34 @@
 using namespace std;
 
 int main(){
-	int n,d,m,l;
+	long long n,d,m,l;
 	cin>>n>>d>>m>>l;
 	//
-	long x=0;
+	long long x=0;
 	long jump=0;
-	bool miss=true;
+	bool miss=true; 
+	long prevB=l;
 	do{
+	x+=d*(long long)((prevB-x)/d);
 	x+=d;
-	++jump;
+	
 	//cout<<"x="<<x<<endl;
 
-	long full=x/m;
+	long long full=x/m;
 	//cout<<"full="<<full<<endl;
 	//cout<<"full*m="<<full*m<<endl;
 	//cout<<"(full*m+l)="<<(full*m+l)<<endl;
 	//cout<<"(1+full)*(m)="<<(1+full)*(m)<<endl;
 	//cout<<"((1+full)*m+l)="<<((1+full)*m+l)<<endl;
-	if((full*m<=x) && (x<=(full*m+l)) && full<=n){
+	if((full*m<=x) && (x<=(full*m+l)) && (full+1)<=n){
 		//cout<<1<<endl;
+		prevB =(full*m+l);
 		continue;
 	} else
 	
-	if((1+full)*(m)<=x && x<=((1+full)*m+l) && full<n){
+	if((1+full)*(m)<=x && x<=((1+full)*m+l) && (full+2)<n){
 		//cout<<1<<endl;
+		prevB =((1+full)*m+l);
 		continue;	
 	}
 	miss=false;
