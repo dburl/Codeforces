5d13bc1529f79bf581249df597246f8073213e47
dburl <burlyaev.dmitry@gmail.com>
2017-01-17 23:46:22 +0100

    18C solved - 20 mins

--- 18/C/main.cpp
+++ 18/C/main.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-int sum(vector<int>::iterator a,vector<int>::iterator b){
-	long tmp=0;
+int sum(vector<long long>::iterator a,vector<long long>::iterator b){
+	long long tmp=0;
 	while(a!=b){
 		tmp+=*a;
 		++a;
@@ -16,21 +16,21 @@ int sum(vector<int>::iterator a,vector<int>::iterator b){
 
 
 int main(){
-	int n;
+	long long n;
 	cin>>n;
-	vector<int> v(n);
-	for (int i=0;i<n;++i){
+	vector<long long> v(n);
+	for (long long i=0;i<n;++i){
 		cin>>v[i];
 	}
 	//
-	int count=0;
+	long long count=0;
 
-	if(!v.empty()){
-		long sumR=sum(v.begin()+1,v.end());
-		long sumL=v[0];
+	if(v.size()>1){
+		long long sumR=sum(v.begin()+1,v.end());
+		long long sumL=v[0];
 		if (sumR==sumL){ ++count;}
 
-		for(int i=1;i<n-1;++i){
+		for(long long i=1;i<n-1;++i){
 			sumL+=v[i]; sumR-=v[i];
 			if (sumR==sumL){ ++count;}
 		}
