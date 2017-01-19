2dfbee3321c624fdf8bc36f46097b6bda3b6bf8e
dburl <burlyaev.dmitry@gmail.com>
2017-01-19 22:39:47 +0100

    18B to fix

--- 18/B/main.cpp
+++ 18/B/main.cpp
@@ -6,36 +6,17 @@ int main(){
 	long long n,d,m,l;
 	cin>>n>>d>>m>>l;
 	//
-	long long x=0;
-	long jump=0;
-	bool miss=true; 
-	long prevB=l;
-	do{
-	x+=d*(long long)((prevB-x)/d);
-	x+=d;
-	
-	//cout<<"x="<<x<<endl;
-
-	long long full=x/m;
-	//cout<<"full="<<full<<endl;
-	//cout<<"full*m="<<full*m<<endl;
-	//cout<<"(full*m+l)="<<(full*m+l)<<endl;
-	//cout<<"(1+full)*(m)="<<(1+full)*(m)<<endl;
-	//cout<<"((1+full)*m+l)="<<((1+full)*m+l)<<endl;
-	if((full*m<=x) && (x<=(full*m+l)) && (full+1)<=n){
-		//cout<<1<<endl;
-		prevB =(full*m+l);
-		continue;
-	} else
-	
-	if((1+full)*(m)<=x && x<=((1+full)*m+l) && (full+2)<n){
-		//cout<<1<<endl;
-		prevB =((1+full)*m+l);
-		continue;	
+	if (d==m){cout<< m*n<<endl;return 0;}
+	else if((d/m)<1){cout<<(1+m/d)*d<<endl;return 0;}
+	else {
+		int hop=l/(d%m);
+		if (hop>=n){
+			cout<<d*(hop+1)<<endl;
+			return 0;
+		} else{
+			cout<<d*(1+(n*m+l)/d)<<endl;
+			return 0;
+		}
 	}
-	miss=false;
-	
-	}while(miss);
-	cout<<x<<endl;
 	return 0;
 }
