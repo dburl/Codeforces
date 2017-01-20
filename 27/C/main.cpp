8c5da1a5174083a90ac6fa51ee2b5ef274878553
dburl <burlyaev.dmitry@gmail.com>
2017-01-21 00:34:16 +0100

    27C to fix

--- 27/C/main.cpp
+++ 27/C/main.cpp
@@ -1,15 +1,53 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+struct T{
+	int id;	
+	int val;
+};
+
+bool compF(T a, T b){return a.val<b.val;}
 int main(){
-	long n;
+	int n;
 	cin>>n;
-	vector<>
-	for (long i=0;)
+	vector<T> v(n);
+	for (int i=0;i<n;++i){
+		cin>>v[i].val;
+		v[i].id=i+1;
+	}
+	//
+	sort(v.begin(),v.end(), compF);
+	for (int i=0;i<n;++i){
+		cout<<"*"<<v[i].id<<" ->"<<v[i].val<<endl;
+	}
+	T a2=v[0];
+	T a1, a3;
+	bool lb=false,rb=false;
+	for (int i=0;i<n;++i){
 
-
+		if((v[i].id>a2.id) && (v[i].val > a2.val)){
+			cout<<"r="<<v[i].id<<endl;
+			a3=v[i];
+			rb=true;
+		} 
+		if((v[i].id<a2.id) && (v[i].val > a2.val)){
+			cout<<"l="<<v[i].id<<endl;
+			a1=v[i];
+			lb=true;
+		}
+		if (lb&&rb){
+			break;
+		}
+	}
+	if (lb&&rb){
+	cout<<3<<endl;
+	cout<<a1.id<<" "<<a2.id<<" "<<a3.id<<endl;
+	}else{
+		cout<<"0"<<endl;
+	}
 
 	return 0;
 }
