0909db5b81fb06b233689f1c59b8fd2de49be23d
dburl <burlyaev.dmitry@gmail.com>
2017-01-21 22:32:29 +0100

    27C to fix

--- 27/C/main.cpp
+++ 27/C/main.cpp
@@ -6,10 +6,11 @@ using namespace std;
 
 struct T{
 	int id;	
-	int val;
+	long val;
 };
 
 bool compF(T a, T b){return a.val<b.val;}
+bool compF2(T a, T b){return a.val>b.val;}
 int main(){
 	int n;
 	cin>>n;
@@ -21,27 +22,55 @@ int main(){
 	//
 	sort(v.begin(),v.end(), compF);
 	for (int i=0;i<n;++i){
-		cout<<"*"<<v[i].id<<" ->"<<v[i].val<<endl;
+		//cout<<"*"<<v[i].id<<" ->"<<v[i].val<<endl;
 	}
-	T a2=v[0];
-	T a1, a3;
-	bool lb=false,rb=false;
-	for (int i=0;i<n;++i){
+	T a1, a2, a3;
+	bool lb,rb;
+	for(int j=0;(j<2)&&(j<n)&&(!(lb&&rb));++j){
+	a2=v[j];
+	lb=false;rb=false;
+		for (int i=0;i<n;++i){
+			if((v[i].id>a2.id) && (a2.val <= v[i].val  )){
+				//cout<<"r="<<v[i].id<<endl;
+				a3=v[i];
+				rb=true;
+			} 
+			if((v[i].id<a2.id) && (a2.val <= v[i].val  )){
+				//cout<<"l="<<v[i].id<<endl;
+				a1=v[i];
+				lb=true;
+			}
+			if (lb&&rb){
+				break;
+			}
+		}
+	}
+	if (!(lb&&rb)){
+		sort(v.begin(),v.end(), compF2);
+		for (int i=0;i<n;++i){
+			//cout<<"-"<<v[i].id<<" ->"<<v[i].val<<endl;
+		}
+		for(int j=0;(j<2)&&(j<n)&&(!(lb&&rb));++j){
+			a2=v[j];	
+			lb=false,rb=false;
+			for (int i=0;i<n;++i){
+				if((v[i].id>a2.id) && (v[i].val <= a2.val)){
+					//cout<<"r="<<v[i].id<<endl;
+					a3=v[i];
+					rb=true;
+				} 
+				if((v[i].id<a2.id) && (v[i].val <= a2.val)){
+					//cout<<"l="<<v[i].id<<endl;
+					a1=v[i];
+					lb=true;
+				}
+				if (lb&&rb){
+					break;
+				}
+			}
+		}
+	}
 
-		if((v[i].id>a2.id) && (v[i].val > a2.val)){
-			cout<<"r="<<v[i].id<<endl;
-			a3=v[i];
-			rb=true;
-		} 
-		if((v[i].id<a2.id) && (v[i].val > a2.val)){
-			cout<<"l="<<v[i].id<<endl;
-			a1=v[i];
-			lb=true;
-		}
-		if (lb&&rb){
-			break;
-		}
-	}
 	if (lb&&rb){
 	cout<<3<<endl;
 	cout<<a1.id<<" "<<a2.id<<" "<<a3.id<<endl;
