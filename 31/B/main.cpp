c814b47bf98d45cdc935d8ddb20ca2cd6c1f82d9
dburl <burlyaev.dmitry@gmail.com>
2017-01-27 23:54:37 +0100

    31B solved - to fix

---
+++ 31/B/main.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+int main(){
+	string s;
+	cin>>s;
+	// 
+	int phase=0;
+	int start=0;
+	vector<string> all;
+	for (int i=0; i<s.size(); ++i){
+		if (phase==0 && s[i]!='@'){continue;}
+  		if (phase==0 && s[i]=='@'){
+			if ((i-start)==0){
+				cout<<"No solution"<<endl;
+				return 0;			
+			}
+			phase=1;
+			continue;
+		}
+		if (phase==1 && s[i]!='@'){
+			all.push_back(s.substr(start,i-start+1)); 
+			phase=0;  
+			start=i+1;
+			continue;
+		}
+	}	
+	if (!all.empty()){
+	for (vector<string>::const_iterator it=all.begin();it!=all.end();++it){
+		cout<<*it;	
+		if( (++it)!=all.end() ){cout<<",";--it;} else{--it;}
+	}
+	cout<<endl; return 0;
+	}
+	cout<<"No solution"<<endl;
+
+	return 0;
+} 
