84af68fbaa8c81ac5d66c88a30ac1ab4c41fe273
dburl <burlyaev.dmitry@gmail.com>
2017-02-10 00:06:17 +0100

    38B solved - 30 mins

--- 38/B/main.cpp
+++ 38/B/main.cpp
@@ -36,15 +36,18 @@ set<pair<int,int> > knight(int x, int y){
 	return tmp;
 }
 
-
+void printSet(set<pair<int, int> > s){
+	for (set<pair<int,int> >::const_iterator it=s.begin();it!=s.end();++it)
+		cout<<it->first<< "."<<it->second<<endl;
+}
 int main(){
 	char a1,a2;
 	int y1, y2, x1, x2;
 	cin>>a1>>y1;
 	cin>>a2>>y2;
 	//cout<<x1<<"!"<<y1;
-	x1=int('a')-((int) a1);
-	x2=int('a')-((int) a2);
+	x1=((int) a1)-(int)'a';
+	x2=((int) a2)-(int)'a';
 	--y1;--y2;
 	//
 	set<pair<int,int> > s;
@@ -54,14 +57,17 @@ int main(){
 		s.insert(make_pair(x1,i));
 		s.insert(make_pair(i,y1));
 	}	
-	cout<< s.size()<<endl;
+	//cout<< s.size()<<endl;
+	//printSet(s);
 	
 	set<pair<int,int> > s1=knight(x2,y2);
 	set<pair<int,int> > s2=knight(x1,y1);
 	s.insert(s1.begin(),s1.end());
 	s.insert(s2.begin(),s2.end());
-	cout<<s1.size()<<endl;
-	cout<<s2.size()<<endl;
+	//cout<<s1.size()<<endl;
+	//printSet(s1);
+	//cout<<s2.size()<<endl;
+	//printSet(s2);
 	cout<<8*8-s.size()<<endl;
 
 	return 0;
