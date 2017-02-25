4c9cc64c0a507d2d3c13fde50f64e48b0f673bed
dburl <burlyaev.dmitry@gmail.com>
2017-02-25 23:55:44 +0100

    50B fixed

--- 50/B/main.cpp
+++ 50/B/main.cpp
@@ -4,35 +4,21 @@
 #include <set>
 using namespace std;
 
-static long fac (long n){
-	long acc=1;
-	for (int i=1;i<=n;++i){
-		acc=acc*i;
-	}
-	return acc;
-}
 int main(){
 	string s;
 	getline(cin,s);
 	//
-	multimap<char,int> m;
+	multimap<char,long> m;
 	set<char> sC;
 	for (int i=0;i<s.size();++i){
 		m.insert(make_pair(s[i],i));
 		sC.insert(s[i]);
 	}
-	long  acc=0;
+	long  long acc=s.size();
 	for (set<char>::const_iterator it =sC.begin();
 		it!=sC.end();++it){
-		int num=m.count(*it);
-		//cout<<num<<endl;
-		//cout<<fac(num)
-		//++acc;
-		if (num>1){
-			acc+=fac(num)/(fac(num-2));
-			acc+=num;
-		} else if (num==1)
-			++acc;
+		long long num=0;
+		acc+=num*num;
 	} 	
 	cout<<acc<<endl;
 
